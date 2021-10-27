9c090e0e32396cd6aeabd7847cfe4c30df10e613
Greg <gbrewton1980@gmail.com>
2021-10-26 19:39:30 -0600

    adds route

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 const express = require("express");
 const server = express();
 const port = 8000;
+const data = require("./data/db");
+
 
 server.get("/", (req, res) => {
   res.send("Hello Express");
 });
+
+server.get("/users", (req, res) => {
+  data.find().then(users => res.status(200).json(users))
+  .catch(() => {
+    res.status(500).send("something broke!")
+  })
+})
 
 server.listen(port, () => {
   console.log(`API running on port ${port}`);
