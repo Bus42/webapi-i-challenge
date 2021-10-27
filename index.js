d606eb88f115700ff22eae1e222c3fa4a373e17e
Greg <gbrewton1980@gmail.com>
2021-10-26 18:56:41 -0600

    creates server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+const server = express();
+const port = 8000;
+
+server.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+server.listen(port, () => {
+  console.log(`API running on port ${port}`);
+});
